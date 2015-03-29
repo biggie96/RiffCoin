a2a372c90a3fe44957b317912a730d8b08050738
Biggie Emmanuel <biggieemmanuel@gmail.com>
2015-03-29 05:43:03 -0400

    got the price!

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 
 @app.route('/requests', methods=['GET'])
 def bandsprice():
+	#Demo
 	r = requests.get("http://perelste.in:8001/api/bands/id/72")
 	mr = jsonify(results = r.json())
 	r = r.json()
@@ -22,15 +23,33 @@ def bandsprice():
 
 	print r[0]['country']
 
+	merch = False
 	for i in range(len(r[0]['related_links'])):
 		print r[0]['related_links'][0]['category']
 		if(r[0]['related_links'][i]['category'] == "Official_Merchandise"):
-			print "yes!"
+			merch = True
 			break
 
-	allMerch = requests.get(r[0]['related_links'][i]['url'])
-	allMerch = allMerch.text
-	print allMerch
+	if(merch):
+		allMerch = requests.get(r[0]['related_links'][i]['url'])
+		allMerch = allMerch.text
+
+		dollas = allMerch.find("$")
+
+		if(dollas != -1):
+			price = 0
+			if(allMerch[dollas + 1].isdigit()):
+				price = price + (10 * int(allMerch[dollas + 1]))
+				print price
+			if(allMerch[dollas + 2].isdigit()):
+				price = price + int(allMerch[dollas + 2])
+				print price
+
+	theCost = json.dumps({"price": price}, sort_keys=True)
+
+	return theCost
+
+	#Experimental
 
 
 
@@ -38,7 +57,6 @@ def bandsprice():
 
 
 
-	return mr
 
 
 	
